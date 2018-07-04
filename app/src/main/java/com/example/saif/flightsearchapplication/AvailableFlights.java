053c0cc1b466aee3f9e8172b1392d14bc7df1e38
Shaik Saif Ali <saifshine7@gmail.com>
2018-07-04 09:00:01 +0530

    Available Flights Activity Created

---
+++ app/src/main/java/com/example/saif/flightsearchapplication/AvailableFlights.java
@@ -0,0 +1,13 @@
+package com.example.saif.flightsearchapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AvailableFlights extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_available_flights);
+    }
+}
