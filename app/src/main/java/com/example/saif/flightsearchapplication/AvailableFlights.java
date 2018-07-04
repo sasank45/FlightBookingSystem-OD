344d7698d0e14e08c0fbfbd1e19ccc7900b6d063
Shaik Saif Ali <saifshine7@gmail.com>
2018-07-05 02:29:10 +0530

    database construction

--- app/src/main/java/com/example/saif/flightsearchapplication/AvailableFlights.java
+++ app/src/main/java/com/example/saif/flightsearchapplication/AvailableFlights.java
@@ -1,7 +1,12 @@
 package com.example.saif.flightsearchapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class AvailableFlights extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class AvailableFlights extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_available_flights);
+    }
+
+    public void confirmationPage(View view) {
+        Intent intent = new Intent(this, ListingActivity.class);
+        Button launchActivity = (Button) findViewById(R.id.searchid);
+        String message = launchActivity.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
