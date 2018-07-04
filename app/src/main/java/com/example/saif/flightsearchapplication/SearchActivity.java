f2fed1f8fe773e5ad51543bda91101b7013b8b06
Shaik Saif Ali <saifshine7@gmail.com>
2018-07-04 20:50:50 +0530

    Selection is enabled

--- app/src/main/java/com/example/saif/flightsearchapplication/SearchActivity.java
+++ app/src/main/java/com/example/saif/flightsearchapplication/SearchActivity.java
@@ -1,5 +1,9 @@
 package com.example.saif.flightsearchapplication;
 
+import java.util.Calendar;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,7 +11,9 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -16,12 +22,23 @@ import java.util.List;
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class SearchActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView;
+    private int year, month, day;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        dateView = (TextView) findViewById(R.id.departid);
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month+1, day);
         Spinner spinner1 = (Spinner) findViewById(R.id.fromid);
         Spinner spinner2 = (Spinner) findViewById(R.id.toid);
         spinner1.setOnItemSelectedListener(this);
@@ -40,14 +57,12 @@ public class SearchActivity extends AppCompatActivity implements AdapterView.OnI
     }
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        // On selecting a spinner item
         String item = parent.getItemAtPosition(position).toString();
 
-        // Showing selected spinner item
         Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
     }
     public void onNothingSelected(AdapterView<?> arg0) {
-        // TODO Auto-generated method stub
+
     }
 
     public void listofflights(View view) {
@@ -57,7 +72,37 @@ public class SearchActivity extends AppCompatActivity implements AdapterView.OnI
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "ca",
+                Toast.LENGTH_SHORT)
+                .show();
+    }
 
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == 999) {
+            return new DatePickerDialog(this,
+                    myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new
+            DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker arg0,
+                                      int arg1, int arg2, int arg3) {
+
+                    showDate(arg1, arg2+1, arg3);
+                }
+            };
+
+    private void showDate(int year, int month, int day) {
+        dateView.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
+    }
 
 }
 
