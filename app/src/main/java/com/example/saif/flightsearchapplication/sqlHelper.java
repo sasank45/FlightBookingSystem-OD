e624d8ef2a0251c236aa426575356efcb75692c8
Shaik Saif Ali <saifshine7@gmail.com>
2018-07-04 11:54:43 +0530

    Database config ready,

--- app/src/main/java/com/example/saif/flightsearchapplication/sqlHelper.java
+++ app/src/main/java/com/example/saif/flightsearchapplication/sqlHelper.java
@@ -1,8 +1,11 @@
 package com.example.saif.flightsearchapplication;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.media.Image;
+import android.util.Log;
 
 public class sqlHelper extends SQLiteOpenHelper {
 
@@ -24,6 +27,30 @@ public class sqlHelper extends SQLiteOpenHelper {
     }
 
     public void createtb(){
-        
+        SQLiteDatabase database = getWritableDatabase();
+        String query="create table if not exists "+tblName +"(id integer primary key AUTOINCREMENT,name text,dprttime text,arvltime text,totldur text,Price text)";
+        database.execSQL(query);
+        Log.d("Table Create",query);
+
     }
+    public void droptb(){
+        SQLiteDatabase database = getWritableDatabase();
+        String query="drop table "+tblName;
+        database.execSQL(query);
+        Log.d("Table Drop",query);
+
+    }
+
+    public void insert(String name, String dprttime, String arvltime, String totldur, String Price){
+        SQLiteDatabase database = getWritableDatabase();
+        String query ="insert into table "+tblName + " (name,dprttime,arvltime,totldur,Price) values ('"+name+"','"+dprttime+"','"+arvltime+"','"+totldur+"','"+Price+"')";
+        database.execSQL(query);
+        Log.d(" tbl insert",query);
+    }
+
+    public Cursor getRow(String id){
+        SQLiteDatabase database=getReadableDatabase();
+        Cursor cursor=database.rawQuery("select name,dprttime,arvltime,totldur,Price from "+tblName+" where id ='"+id+"'",null);
+    }
+
 }
