529584fbd6ec1405a97efa969be314e98d129574
Shaik Saif Ali <saifshine7@gmail.com>
2018-07-04 15:22:15 +0530

    Launched another activity! Worked

--- app/src/main/java/com/example/saif/flightsearchapplication/SearchActivity.java
+++ app/src/main/java/com/example/saif/flightsearchapplication/SearchActivity.java
@@ -1,7 +1,13 @@
 package com.example.saif.flightsearchapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -9,6 +15,14 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+    }
+
+    public void listofflights(View view) {
+        Intent intent = new Intent(this, AvailableFlights.class);
+        Button launchActivity = (Button) findViewById(R.id.searchid);
+        String message = launchActivity.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
 
